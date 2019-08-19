afa755f20690c245f9b8a9c84637979842c0afd3
SB <admin@openwifi.su>
2019-08-20 00:52:06 +0500

    fix

--- fdx/urls.py
+++ fdx/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from fdx_search.views import home, search
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
--- fdx_search/views.py
+++ fdx_search/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     ctx = {}
-    return render()
+    return render(request, 'fdx_search/index.html',ctx)
+
+def search(request):
+    ctx = {}
+    return render(request, 'fdx_search/search.html',ctx)
+
